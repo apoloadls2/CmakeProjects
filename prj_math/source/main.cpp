cee658211ac7e121082608dc777f0cb73b85db9c
apoloadls2 <apolo.silva@academico.ifpb.edu.br>
2022-09-20 23:37:02 -0300

    ssss

---
+++ prj_math/source/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include "gcem.hpp"
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int main(){
+
+  cout << "Math 1 : " << gcem::cos(2) << endl;
+
+  return 0;
+}
