9b4695bcbadbeab52df6033b6fa0b7884f03db02
apoloadls2 <apolo.silva@academico.ifpb.edu.br>
2022-10-27 19:37:10 -0300

    CMakePj2 add

---
+++ CMakePj2/source/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+    string c = "";
+    
+    do{
+    cin >> c;
+    cout << "\nword : " << c << endl;
+    }while(c != "exit");
+
+
+    return 0;
+}
